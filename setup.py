2073cc1b3ecffa80c7bc6c1565367e1371693f3a
Eric Davis <ed@npri.org>
2011-11-20 15:56:25 -0800

    Drop setup_requires on setuptools_git

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
     zip_safe             = False,
     install_requires     = ['setuptools'],
     include_package_data = True,
-    setup_requires       = ['setuptools_git'],
     classifiers = [
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
